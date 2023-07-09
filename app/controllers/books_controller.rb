83096314e148e056b6b65939d6974bc38e929462
HanaHarada <129660325+hana5555@users.noreply.github.com>
2023-07-09 13:27:16 +0000

    [Add]books/destroy,books/edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,18 +17,28 @@ class BooksController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @book = Book.new
     @books = Book.find(params[:id])
+    @user = @books.user
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @books = Book.find(params[:id])
+    @books.destroy
+    redirect_to books_path
   end
 
   private
